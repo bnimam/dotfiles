81130eb3bf015cb3a17f2db0d3467a2d6d1ace63
Brannon Imamura <brannon@geospace.io>
2026-06-02 21:51:11 +0000

    nvim fixes

--- nvim/init.lua
+++ nvim/init.lua
@@ -55,3 +55,33 @@ require("configs.float-term")
 -- Better mouse scrolling
 vim.api.nvim_set_keymap("n", "<ScrollWheelUp>", "<C-y>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<ScrollWheelDown>", "<C-e>", { noremap = true, silent = true })
+
+-- Highlight other occurences of word on cursor
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.server_capabilities.documentHighlightProvider then
+            vim.api.nvim_create_autocmd("CursorHold", {
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.document_highlight()
+                end,
+            })
+            vim.api.nvim_create_autocmd("CursorMoved", {
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.clear_references()
+                end,
+            })
+        end
+    end,
+})
+
+-- Adds line moving / swapping like in vscode
+-- Move line up and down (Normal mode)
+vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
+
+-- Move selection up and down (Visual mode)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
